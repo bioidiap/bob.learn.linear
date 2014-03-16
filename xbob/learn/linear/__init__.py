e8ce8a79eb874e66dca043dc256864624addc099
Andre Anjos <andre.dos.anjos@gmail.com>
2014-03-16 22:23:32 +0100

    Fix documentation generation

--- xbob/learn/linear/__init__.py
+++ xbob/learn/linear/__init__.py
@@ -5,3 +5,7 @@ def get_include():
   """Returns the directory containing the C/C++ API include directives"""
 
   return __import__('pkg_resources').resource_filename(__name__, 'include')
+
+# gets sphinx autodoc done right - don't remove it
+__all__ = [k for k in dir() if not k.startswith('_')]
+del k
